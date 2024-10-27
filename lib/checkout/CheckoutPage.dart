dd97173dbfe4cab580b65992b30b98ba246dc9d8
Kavinda Botheju <kavinda.botheju@grubtech.com>
2024-10-27 11:27:10 +0530

    add checkout

--- lib/checkout/CheckoutPage.dart
+++ lib/checkout/CheckoutPage.dart
@@ -1,11 +1,10 @@
-// ignore_for_file: use_key_in_widget_constructors
+// checkout_page.dart
 
 import 'package:flutter/material.dart';
-import 'package:modern_landscaping/checkout/CheckoutPage.dart';
 import 'package:provider/provider.dart';
 import 'package:modern_landscaping/provider/CartProvider.dart';
 
-class Cart extends StatelessWidget {
+class CheckoutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cartProvider = Provider.of<CartProvider>(context);
@@ -13,22 +12,10 @@ class Cart extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Cart',
+          'Checkout',
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
         ),
         backgroundColor: Colors.white,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.arrow_forward, color: Colors.black),
-            onPressed: () {
-              // Navigate to checkout page
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => CheckoutPage()),
-              );
-            },
-          ),
-        ],
       ),
       backgroundColor: Colors.grey[100],
       body: cartProvider.distinctProductCount > 0
@@ -56,6 +43,36 @@ class Cart extends StatelessWidget {
           : Center(
               child: Text('Your cart is empty!'),
             ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(16.0),
+        child: ElevatedButton(
+          onPressed: () {
+            // Proceed to payment or confirmation logic
+            // Here, you can implement your payment processing logic
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('Checkout Successful'),
+                content: Text('Thank you for your purchase!'),
+                actions: [
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop(); // Go back to cart page
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+          child: Text('Proceed to Payment'),
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            backgroundColor: Colors.green, // Button color
+          ),
+        ),
+      ),
     );
   }
 }
